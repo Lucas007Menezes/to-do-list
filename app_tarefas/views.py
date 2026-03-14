6e1306e76d4b4c35cfb18a37627c0d9fda77ffe9
Lucas Menezes <llucas007menezes@gmail.com>
2026-03-13 21:02:07 -0300

    feat: front-end

--- app_tarefas/views.py
+++ app_tarefas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Tarefa
 
 # Create your views here.
+
+
+def listar_tarefas(request):
+    minhas_tarefas = Tarefa.objects.all()
+
+    contexto = {
+        'tarefas': minhas_tarefas
+    }
+
+    return render(request, 'lista.html', contexto)
